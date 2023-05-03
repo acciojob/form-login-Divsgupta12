2a6b8c507bf76434c6d34c137bf9f945d2e8214f
Divya Gupta <117669677+Divsgupta12@users.noreply.github.com>
2023-05-03 20:44:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function getFormvalue() {
-    //Write your code here
-
+    const fname = document.querySelector("#fname").value;
+	const lname = document.querySelector("#lname").value;
+	alert(fname+" "+ lname);
 }
